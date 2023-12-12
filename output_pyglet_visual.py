a2a3330d5005dc24a928860db1576a6476291101
XIYANG1018 <pkuyangxi@163.com>
2023-12-11 17:32:31 -0800

    resized and relocated

--- output_pyglet_visual.py
+++ output_pyglet_visual.py
@@ -8,9 +8,44 @@ from pyglet import image
 
 # global
 window = pyglet.window.Window(resizable=True)
+print(window.width)
+print(window.height)
+# bgfiller = pyglet.shapes.Rectangle()
+bgimage_path = 'tileImages/mahjongbg.jpg'
+bgimage = pyglet.image.load(bgimage_path)
+bg_sprite = pyglet.sprite.Sprite(bgimage)
 batch = pyglet.graphics.Batch()
-imageWidth = 50
-imageHeight = 75
+imageWidth = 35
+imageHeight = 55
+
+
+text_label = pyglet.text.Label(
+    'Tiles to discard:',
+    font_name='Times New Roman',
+    font_size=18,
+    x=50, y=450,
+    anchor_x='left', anchor_y='baseline'
+)
+
+
+text_label_2 = pyglet.text.Label(
+    'Effective Tiles to get:',
+    font_name='Times New Roman',
+    font_size=18,
+    x=50, y=300,
+    anchor_x="left", anchor_y="baseline"
+)
+
+text_label_3 = pyglet.text.Label(
+    'Total Number of Effective Tiles:',
+    font_name='Times New Roman',
+    font_size=18,
+    x=50, y=500,
+    anchor_x="left", anchor_y="baseline"
+)
+
+
+
 tile_dic = {1:"1 Man", 2:"2 Man", 3:"3 Man", 4:"4 Man", 5:"5 Man", 6:"6 Man", 7:"7 Man", 8:"8 Man", 9:"9 Man", 
             11:"1 So", 12:"2 So",13:"3 So",14:"4 So",15:"5 So",16:"6 So",17:"7 So",18:"8 So", 19:"9 So", 
             21:"1 Pin", 22:"2 Pin", 23:"3 Pin", 24:"4 Pin", 25:"5 Pin", 26:"6 Pin", 27:"7 Pin", 28:"8 Pin", 29:"9 Pin", 
@@ -21,7 +56,15 @@ values_list = []
 for item in result_dict.values():
     values_list.append(item['value'])  
 
-max_value = max(values_list) 
+max_value = max(values_list)
+
+text_label_4 = pyglet.text.Label(
+    f'{max(values_list)}',
+    font_name='Times New Roman',
+    font_size=18,
+    x=400, y=500,
+    anchor_x="left", anchor_y="baseline"
+)
 
 # Find all keys with the maximum 'value'
 keys_with_max_value = []
@@ -32,13 +75,13 @@ for key, val in result_dict.items():
 
 img_list = []
 def display_img():
-    xd = 0
-    yd = 0
+    xd = 20
+    yd = 350
     for idx, key in enumerate(keys_with_max_value):
         img = pyglet.resource.image(f"tileImages/{key}.png")
         img.width = imageWidth
         img.height = imageHeight
-        sprite = pyglet.sprite.Sprite(img = img, x = xd, y = window.height//3 + img.height * idx, batch = batch)
+        sprite = pyglet.sprite.Sprite(img = img, x =window.width//3+idx*img.width, y = yd, batch = batch)
         img_list.append(sprite)
         # label_x = xd + img.width + 10  # Adjust label position relative to the image sprite
         # label_y = window.height // 3 + img.height * idx + img.height // 2  # Center label vertically
@@ -48,6 +91,7 @@ def display_img():
 def display_tile():
         if keys_with_max_value:
             max_value_key = keys_with_max_value[0]
+            print(max_value_key)
             tiles_list = result_dict[max_value_key]['tiles']
             print("@@@",tiles_list)
 
@@ -59,7 +103,7 @@ def display_tile():
             largest_value_img.width = imageWidth
             largest_value_img.height = imageHeight
             # tile_sprite = pyglet.sprite.Sprite(img = tile_img, x = tile_img.width + (idx * tile_img.width), y= tile_img.height, batch = batch)
-            tile_sprite = pyglet.sprite.Sprite(img=tile_img, x= idx * tile_img.width + tile_img.width, y=window.height//3 + tile_img.height, batch=batch)
+            tile_sprite = pyglet.sprite.Sprite(img=tile_img, x= idx * tile_img.width + tile_img.width, y=window.height//5 + tile_img.height, batch=batch)
             img_list.append(tile_sprite)
             # img_list.append(tile_sprite)
 
@@ -67,6 +111,11 @@ def display_tile():
 @window.event
 def on_draw():
     window.clear()
+    bg_sprite.draw()
+    text_label.draw()
+    text_label_2.draw()
+    text_label_3.draw()
+    text_label_4.draw()
     for sprite in img_list:
         sprite.draw()
     

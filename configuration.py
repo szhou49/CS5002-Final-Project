8406fe81a3df1e775cf92a9de81d67dcecf9028c
Xi Yang <117253597+XIYANG1018@users.noreply.github.com>
2023-12-12 07:34:16 -0800

    Add files via upload

--- configuration.py
+++ configuration.py
@@ -1,6 +1,8 @@
 import input_validation
 import ukerie_calculator
 import shanten_calculator
+import probability_pon
+import probability_kong
 
 hand_to_check = input_validation.hand_to_check
 tiles_on_table = input_validation.tiles_on_table
@@ -21,3 +23,13 @@ remainingTiles = [x - y - w for x, y, w in zip(all_tiles, hand_to_check, tiles_o
 value_tiles_dic = ukerie_calculator.calculateDiscardUkeire(hand_to_check, remainingTiles)
 
 minimun_shanten = shanten_calculator.calculateMinimumShanten(hand_to_check)
+
+
+probability_pon.find_pairs(hand_to_check)
+probability_pon.get_remaining_pair_tile()
+odd_pon = probability_pon.odd_pon()
+
+
+probability_kong.find_kongs(hand_to_check)
+probability_kong.get_remaining_kong_tile()
+odd_kong = probability_kong.odd_kong()

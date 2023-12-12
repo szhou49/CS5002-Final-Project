aaa08c97b956db3a35ebe74ae9b91050308835fa
Songling Zhou <songlingzhou04@gmail.com>
2023-12-12 14:15:32 +0800

    test

--- output_visuals.py
+++ output_visuals.py
@@ -20,13 +20,33 @@ def show_result(list):
     plt.style.use('_mpl-gallery')
     fig, ax = plt.subplots()
     non_zero_x = []
-
+    x_label = []  
     x = 0 + np.arange(38)
 
     for i in range(len(list)):
-        if list[i] != 0:
-            ax.plot(i, list[i])
-            non_zero_x.append(x[i])
+        if list[i] == 0:
+            continue
+        non_zero_x.append(x[i])
+        if 0 <= i <= 8:
+            x_label.append(str(i + 1) + "m")
+        elif 9 <= i <= 17:
+            x_label.append(str(i - 8) + "s")
+        elif 18 <= i <= 26:
+            x_label.append(str(i - 17) + "p")
+        elif i == 27:
+            x_label.append("East")
+        elif i == 28:
+            x_label.append("South")
+        elif i == 29:
+            x_label.append("West")
+        elif i == 30:
+            x_label.append("North")
+        elif i == 31:
+            x_label.append("Red")
+        elif i == 32:
+            x_label.append("White")
+        elif i == 33:
+            x_label.append("Green")
 
     for i in range(len(list)):
         if list[i] != 0:
@@ -34,10 +54,9 @@ def show_result(list):
             plt.setp(stemlines, 'linewidth', 15)
             plt.setp(markerline, markersize=25)
 
-
     ax.set_ylim(bottom=0)
     ax.set_xticks(non_zero_x)
     ax.set_yticks(list)
-    ax.set_xticklabels(non_zero_x)
+    ax.set_xticklabels(x_label)
     ax.set_yticklabels(list)
     plt.show()
